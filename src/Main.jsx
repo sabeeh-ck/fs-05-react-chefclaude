17d0ca0e347f1b4bc494c6b7a6855ff6a9000401
sabeeh <sabeehck02@gmail.com>
2025-10-26 23:45:15 +0530

    Commit 5.1

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,6 @@ export default function Main() {
 
     const addIngredient = (formData) => {
         const newIngredient = formData.get("ingredient");
-
         setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
     };
 
@@ -15,12 +14,23 @@ export default function Main() {
                 <input type="text" aria-label="Add ingredient" placeholder="e.g. oregano" name="ingredient" />
                 <button>+ Add ingredient</button>
             </form>
-
-            <ul>
-                {ingredients.map((ingredient) => (
-                    <li key={ingredient}> {ingredient} </li>
-                ))}
-            </ul>
+            {ingredients.length > 0 ? (
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul>
+                        {ingredients.map((ingredient) => (
+                            <li key={ingredient}> {ingredient} </li>
+                        ))}
+                    </ul>
+                    <div className="get-recipe-container">
+                        <div>
+                            <h3>Ready for a recipe?</h3>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
+                    </div>
+                </section>
+            ) : null}
         </main>
     );
 }
